b088bb956e4a78bbfaf01c18b7548c6f551fd1d6
Shiny <taran.anderson07@gmail.com>
2026-02-13 07:05:40 -0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/djxxlpzh7/video/upload/v1770965109/Waiting_In_Vain_bhc44j.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/djxxlpzh7/video/upload/v1770987877/Waiting_In_Vain_Bob_Marley__One_Love_-_Music_Inspired_By_The_Film_pk2fow.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
